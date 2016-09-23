011b971bb1720d9f9c263f7cb2fe83bdde1347ba
Evan Stewart <stewarea@gmail.com>
2016-09-23 08:20:04 -0700

    initial setup with a few specs

---
+++ app/app.php
@@ -0,0 +1,9 @@
+
+<?php
+ require_once __DIR__."/../vendor/autoload.php";
+ require_once __DIR__."/../src/Allergies.php";
+ date_default_timezone_set('America/Los_Angeles');
+
+
+$app = new Silex\Application();
+ ?>
